3999335e397318469d3a98c8d0cc1414233c8f90
Tamara Džambić <96500897+TamaraDzambic@users.noreply.github.com>
2024-04-26 13:21:42 +0200

    Spring Boot project initialisation

---
+++ Project/backend/model/src/main/java/com/example/model/User.java
@@ -0,0 +1,62 @@
+package com.example.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
+
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Integer id;
+    @Column(unique = true)
+    private String username;
+    @Column
+    private String firstname;
+    @Column
+    private String lastname;
+    @Column
+    private String telephoneNumber;
+    @Column
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    @Column
+    private boolean isActive;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
