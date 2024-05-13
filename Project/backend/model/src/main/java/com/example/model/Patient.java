bd3aa65779c2ee98b6d682b38fb89e50e82d302a
Tamara Džambić <96500897+TamaraDzambic@users.noreply.github.com>
2024-05-13 18:19:28 +0200

    Fixed patient model

--- Project/backend/model/src/main/java/com/example/model/Patient.java
+++ Project/backend/model/src/main/java/com/example/model/Patient.java
@@ -1,17 +1,31 @@
 package com.example.model;
 
 import com.example.model.enums.Role;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
 @Table(name = "patients")
 @Data
 public class Patient extends User{
+
+
+    @Column
+    private LocalDate birthDate;
+    @Column
+    private String gender;
+    @Column
+    private Double height;
+    @Column
+    private Double weight;
+    @Column
+    private String bloodType;
 
     @OneToMany(mappedBy = "patient")
     private List<BloodTestAnalysis> bloodTestAnalyses;
